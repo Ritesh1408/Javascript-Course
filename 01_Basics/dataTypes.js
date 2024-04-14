5c0c3dcaa00fdaef370f9cfb5fd9cb273f91b6e1
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-04-14 08:49:34 +0000

    container config done

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") //we are using node js not Browser
+
+console.log(3 + 3)
+
+console.log("ritesh")
+
+let name = "ritesh"
+let age  = 18
+let isLogged = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+
+//null => standalone value
+//undefined
+// symbol => unique
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
