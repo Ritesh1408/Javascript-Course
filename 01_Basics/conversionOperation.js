64ec0265389317b9378327b46ecf6acd4a7beb4e
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-04-15 10:09:18 +0000

    Operation config done

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -1,15 +1,15 @@
 let score = 33
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 let num = "33abc"
 
 let valueNUm = Number(num);
 
-console.log(typeof valueNUm);
-console.log(valueNUm);
+// console.log(typeof valueNUm);
+// console.log(valueNUm);
 
 // "33" ==> 33
 // "33BC" => NaN
@@ -17,7 +17,7 @@ console.log(valueNUm);
 
 let isLoggedIn = 1
 let booleanLoggedIn = Boolean(isLoggedIn);
-console.log(booleanLoggedIn);
+// console.log(booleanLoggedIn);
 
 // 1 => true; 
 // "" => false
@@ -28,8 +28,34 @@ console.log(booleanLoggedIn);
 let numb = 44
 let stringNum = String(numb);
 
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+// ******************* Operations ******************//
+
+let value = 4
+let negVal = -value
+// console.log(negVal);
+
+let str1 = "Ritesh"
+let str2 = "Roushan"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(true+);
+
+
+
+
 
 
 
