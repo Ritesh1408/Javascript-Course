e65eb58305f76fcb20e856024082f7d0fd3ed53f
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-04-15 06:03:41 +0000

    conversion of data types

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,35 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let num = "33abc"
+
+let valueNUm = Number(num);
+
+console.log(typeof valueNUm);
+console.log(valueNUm);
+
+// "33" ==> 33
+// "33BC" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
+
+// 1 => true; 
+// "" => false
+// "hitesh" => true
+
+//string conversion 
+
+let numb = 44
+let stringNum = String(numb);
+
+console.log(stringNum);
+console.log(typeof stringNum);
+
+
+
