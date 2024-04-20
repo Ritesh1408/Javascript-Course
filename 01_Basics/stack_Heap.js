078a82002f9c99436cb0551c9381d79b31ea386f
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-04-20 14:00:32 +0000

    memory config done

--- 01_Basics/stack_Heap.js
+++ 01_Basics/stack_Heap.js
@@ -1,4 +1,24 @@
-console.log("Hello world");
+// *********** Memory in data types**************
+
+// stack(Primitive), Heap (Non - primitive)
+// copy of value , Refrence of original value
+
+let myName = "rroushan"
+
+let anothername = myName
+anothername = "Chai or code"
+console.log(myName);
+console.log(anothername);
 
 
+let user = {
+    email: "user@gmail.com",
+    upi: "a@ybl"
+}
+
+let user2 = user
+user2.email = "ritesh@gmail.com"
+
+console.log(user.email);
+console.log(user2.email);
 
