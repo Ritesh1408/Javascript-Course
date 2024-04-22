1ffec7e68d967d17324e4a071c24ddf746a40d18
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-04-22 06:14:07 +0000

    DateTime config improve

--- 01_Basics/07_Dates.js
+++ 01_Basics/07_Dates.js
@@ -1,27 +1,27 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate);
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toJSON());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate); // object type
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate); // object type
 
 // let createDate = new Date(2024, 0, 25)
 // let createDate = new Date(2024, 0, 25, 5, 4)
 let createDate = new Date("2024-01-14")
 
-// console.log(createDate.toDateString());
-// console.log(createDate.toLocaleString());
+console.log(createDate.toDateString());
+console.log(createDate.toLocaleString());
 
 let myTimeStamp = Date.now();
 
-// console.log(myTimeStamp);
-// console.log(createDate.getTime());
-// console.log(Math.floor(Date.now()/ 1000));
+console.log(myTimeStamp);
+console.log(createDate.getTime());
+console.log(Math.floor(Date.now()/ 1000));
 
 let newDate = new Date()
 console.log(newDate);
