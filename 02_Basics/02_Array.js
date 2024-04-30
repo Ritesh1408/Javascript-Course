bc91a4e448e24f8413f4bde8f62755889439665e
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-04-30 11:28:53 +0000

    config done

--- 02_Basics/02_Array.js
+++ 02_Basics/02_Array.js
@@ -3,19 +3,19 @@
 const marvel_heroes = ["Thor", "IronMan", "SpiderMan"]
 const DC = ["SuperMan", "Flash", "Batman"]
 
-// marvel_heroes.push(DC)
+marvel_heroes.push(DC)
 
-// console.log(marvel_heroes);
-// console.log(marvel_heroes[3][1]);
+console.log(marvel_heroes);
+console.log(marvel_heroes[3][1]);
 
-// marvel_heroes.concat(DC)
+marvel_heroes.concat(DC)
 const newHero = marvel_heroes.concat(DC)
-// console.log(newHero);
-// console.log(marvel_heroes);
+console.log(newHero);
+console.log(marvel_heroes);
 
 //spread method to add two arrays
 const allNewHeros = [...marvel_heroes, ...DC]
-// console.log(allNewHeros);
+console.log(allNewHeros);
 
 const img_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 const real_Array = img_array.flat(Infinity)
