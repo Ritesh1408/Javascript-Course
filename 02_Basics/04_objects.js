53b7cff6991ebee4c88425a2b5cbf3b3abbf4f60
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-09-10 14:59:59 +0000

    initial

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,30 @@
+// const tinder = new Object(); // singleton
+
+const tinder_1 = {}; // non singleton
+
+tinder_1.id = "123abc"
+tinder_1.name = "Sam"
+tinder_1.isLoggedIn = false;
+
+
+// console.log(tinder_1);
+// tinder_1.isLoggedIn = true;
+
+// console.log(tinder_1);
+
+const user = {
+    email : "abc@gmail.com",
+    fullname : {
+        userFullname : {
+            firstname : "ritesh",
+            lastname : "roushan"
+        }
+    }
+
+}
+
+console.log(user.fullname.userFullname.firstname);
+
+
+
+
