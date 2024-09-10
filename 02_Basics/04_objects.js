582efaf64c1dcc179f2f8ccfdc455e23017b007d
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-09-10 15:48:43 +0000

    final

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -12,6 +12,7 @@ tinder_1.isLoggedIn = false;
 
 // console.log(tinder_1);
 
+//we can use as nested object
 const user = {
     email : "abc@gmail.com",
     fullname : {
@@ -23,7 +24,56 @@ const user = {
 
 }
 
-console.log(user.fullname.userFullname.firstname);
+// console.log(user.fullname.userFullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+// const obj3 = { obj1, obj2 }
+
+// console.log(obj3);
+
+// using assign method, we can put the source object in the target object
+// {} using this can confirm that output should returned in the target object.
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+
+// using spread operator to merge the object
+const obj3 = {...obj1, ...obj2};
+// console.log(obj3);
+
+// array like 
+
+const users = [
+    {
+        id:1,
+        email : "abcd@gmail.com"
+    }
+]
+
+// users[1].email
+
+console.log(tinder_1);
+
+// we can use keys and values, where the op is Array
+
+console.log(Object.keys(tinder_1));
+console.log(Object.values(tinder_1));
+console.log(Object.entries(tinder_1));
+
+console.log(tinder_1.hasOwnProperty('isLoggedIn'));
+
+console.log('name' in tinder_1);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
