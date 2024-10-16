91222a1a829afaf409d551a22507ad5dcd00e873
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-10-16 16:49:36 +0000

    switch done

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,37 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "Jan"
+
+switch (month) {
+    case "Jan":
+        console.log("Janaury");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+
+    default:
+        console.log("Use valid keywords");
+        
+        break;
+}
+
+
+
+
