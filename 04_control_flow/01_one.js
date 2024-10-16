f4d3fd353cda1f13d8eb12823354e90746752a09
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-10-16 16:34:22 +0000

    if else done

--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -8,11 +8,53 @@ if(false){} // dont run
 
 let i = 0;
 if(i>= 0){
-    console.log(i);
+    // console.log(i);
     
     i++;
 }
-console.log(i);
+// console.log(i);
+//  node 04_control_flow/01_one.js
+
+if(2 == 2){}
+
+const isUserLoggedIn = true;
+
+if(isUserLoggedIn){}
+
+// <,>, <=, >=, ==, !=, === --- this will check data types too, !== -- check negative sign too
+
+let temprature = 41
+if(temprature < 50){
+    // console.log("temprature is less than 50");
+    
+}else{
+    // console.log("temprature is greatre than 50");
+    
+}
+
+const score = 220
+if(score > 100){
+    const power = "fly"
+    // console.log(`User power : ${power}`);
+    
+}
+// console.log(`User power : ${power}`);
+
+const bal = 500
+// if(bal > 1000) console.log("test");
+
+const loogedInfromGoogle = true
+const loggedInfromemail = true
+
+if(loogedInfromGoogle || loggedInfromemail || guesUser){
+    console.log("user Logged In");
+    
+}
+
+
+
+
+
 
 
 
