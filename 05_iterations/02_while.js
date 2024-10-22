044ab15d86b417ba4fd30f434443d4dd2c3d4623
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-10-22 11:16:28 +0000

    while commit

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,25 @@
+//while loop
+
+let index = 0;
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index += 2;
+    
+}
+
+let arr = ['falsh', 'batman', 'iron man']
+
+let i = 0;
+while(i < arr.length){
+    console.log(`value of arr is ${arr[i]}`);
+    i++;
+}
+
+// do while
+// work first then condition check 
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+    
+} while (score <= 10);
