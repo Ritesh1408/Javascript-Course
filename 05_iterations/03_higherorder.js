7478d7fa4bb0937deb2c80da9458f13970c4eac1
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2024-10-22 12:03:46 +0000

    initial commit

---
+++ 05_iterations/03_higherorder.js
@@ -0,0 +1,24 @@
+// for of loop
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+//for of loop
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+
+const greet = "Hi, there!!!"
+for (const i of greet) {
+    console.log(i);
+    
+}
+
+
+
+
+
